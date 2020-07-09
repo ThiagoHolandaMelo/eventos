b375a996fc22760e1144821f0382100db82b021f
Thiago Melo <thiagomelo@edu.unifor.br>
2020-07-08 23:42:14 -0300

    finalizando a quarta aula com inicialização da tela de login

---
+++ src/view/login/index.js
@@ -0,0 +1,12 @@
+//Adicionando o módulo 'react', localizado na pasta node_modules e atrelando a variárive 'React'
+import React from 'react';
+
+
+function Login(){
+    return(
+        <h1>Tela de Login</h1>
+    )
+}
+
+//Função default que vai ser executada inicialmente
+export default Login;
