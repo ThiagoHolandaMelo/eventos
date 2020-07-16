73afe8ba92dc580b1c1b137b172ba9ecd0dc14a1
Thiago Melo <thiagomelo@edu.unifor.br>
2020-07-16 00:33:46 -0300

    Realizando integração da aplicação com o sistema de autenticação do firebase

--- src/view/login/index.js
+++ src/view/login/index.js
@@ -1,27 +1,46 @@
 //Adicionando o módulo 'react', localizado na pasta node_modules e atrelando a variárive 'React'
-import React from 'react';
-import './login.css'
-
+import React, {useState} from 'react';
+import './login.css';
+import firebase from '../../config/firebase';
+import 'firebase/auth';
 
 function Login(){
+
+    const [email, setEmail] = useState();
+    const [senha, setSenha] = useState();
+
+    function logar(){
+    
+        // .then é o conceito de promisse
+        firebase.auth().signInWithEmailAndPassword( email, senha).then( resultado => {
+            //entra aqui em caso de sucesso
+            alert("Login realizado com sucesso");
+        } ).catch( erro => {
+            alert(erro);
+        } )
+        
+    }
 
     //Retorna um conteudo JSX
     //d-flex do boostrap, onde é condensado tudo em caixas
     //mx-auto para centralizar
     //my-2 buscar spacing no boostrap
 
+    //a variavel 'e' simboliza o componente input
+
     return(
         <div className="login-content d-flex align-items-center">
             <form className="form-signin mx-auto">
                 <div className="text-center mb-4">
                     <img className="mb-4" src="/docs/4.5/assets/brand/bootstrap-solid.svg" alt="" width="72" height="72"/>
-                    <h1 className="h3 mb-3 font-weight-normal text-white font-weight-bold">Login</h1>                
+    <h1 className="h3 mb-3 font-weight-normal text-white font-weight-bold">Login</h1>                
                 </div>
 
-                <input type="email" id="inputEmail" className="form-control my-2" placeholder="Email"/>                
-                <input type="password" id="inputPassword" className="form-control my-2" placeholder="Senha"/>                
                 
-                <button class="btn btn-lg btn-login btn-block" type="submit">Sign in</button>
+                <input onChange={ (e) => setEmail(e.target.value)} type="email" id="inputEmail" className="form-control my-2" placeholder="Email"/>                
+                <input onChange={ (e) => setSenha(e.target.value)} type="password" id="inputPassword" className="form-control my-2" placeholder="Senha"/>                
+                
+                <button onClick={logar} class="btn btn-lg btn-login btn-block" type="button">Logar</button>
 
                 <div className="msg-login text-white text-center my-5">
                     <span><strong>WOW!</strong> Você está conectado! &#128526;</span>
