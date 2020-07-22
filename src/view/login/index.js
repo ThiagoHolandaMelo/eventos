03fbebacecf6ed4cacc9648bf7f87e6f6657adf1
Thiago Melo <thiagomelo@edu.unifor.br>
2020-07-21 21:09:42 -0300

    Definindo rotas e navegação em nossa aplicação

--- src/view/login/index.js
+++ src/view/login/index.js
@@ -1,6 +1,8 @@
 //Adicionando o módulo 'react', localizado na pasta node_modules e atrelando a variárive 'React'
 import React, {useState} from 'react';
 import './login.css';
+import {Link} from 'react-router-dom';
+
 import firebase from '../../config/firebase';
 import 'firebase/auth';
 
@@ -52,7 +54,8 @@ function Login(){
                 <div className="opcoes-login mt-5 text-center">
                     <a href="#" className="mx-2"> Recuperar Senha</a>
                     <span className="text-white">&#9733;</span>
-                    <a href="#" className="mx-2">Quero Cadastrar</a>
+                    <Link to='novousuario' className='mx-2'>Quero Cadastrar</Link>
+                    {/* <a href="#" className="mx-2">Quero Cadastrar</a> */}
                 </div>
                
             </form>
